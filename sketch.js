8c025aff79d9f9ba3b18cd094d691fa3b446d9d8
Arnit123 <72302772+Arnit123@users.noreply.github.com>
2020-11-18 22:09:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+/*mam dont know why its showing error plz check i have not even started properly yet
+and its showing error...*/
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+function setup() {
+  createCanvas(800,400);
+  ground1 = new Ground(400, 370, 900, 20);
+}
+
+function draw() {
+  engine = Engine.create();
+  world = engine.world
+  background(255,255,255);  
+  drawSprites();
+}
